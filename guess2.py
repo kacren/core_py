6f455e1c78e83d03e3ce36c3a55eecee39d6db4b
Mr.chen <chenkai9433@163.com>
2018-11-10 15:55:20 +0800

    猜数游戏

---
+++ guess2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import random
+
+num = random.randint(1,100)
+
+counter = 0
+
+while counter < 5:
+    answer = int(input('guess the number:'))
+    if answer > num:
+        print('猜大了')
+    elif answer < num:
+        print('猜小了')
+    else:
+        print('猜对了')
+        break
+    counter += 1
+else: # 循环被break就不执行了，没有被break才执行
+    print('the number is:',num)
